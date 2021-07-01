4b3c7ebffe36d552690f4232af85cee2399234e9
Vanessa_Riquelme <vriquelmefe@gmail.com>
2021-06-30 23:16:53 -0400

    probando codigo

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var _cliente = _interopRequireDefault(require("./src/cliente.js"));
+var _cliente = _interopRequireDefault(require("./cliente.js"));
 
-var _impuestos = _interopRequireDefault(require("./src/impuestos.js"));
+var _impuestos = _interopRequireDefault(require("./impuestos.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
--- src/cliente.js
+++ src/cliente.js
@@ -1,16 +1,16 @@
-export default class Cliente {
-  constructor(nombre, impuesto) {
-    this._nombre = nombre;
-    this._impuesto = impuesto;
-  }
-  get nombre() {
-    return this._nombre;
-  }
-  set nombre(nuevoNombre) {
-    this._nombre = nuevoNombre;
-  }
-  calcularImpuesto() {
-    return (this._impuesto._monto_bruto_anual - this._impuesto._deducciones) * 0.21;
-  }
+  export default class Cliente {
+    constructor(nombre, impuesto) {
+      this._nombre = nombre;
+      this._impuesto = impuesto;
+    }
+    get nombre() {
+      return this._nombre;
+    }
+    set nombre(nuevoNombre) {
+      this._nombre = nuevoNombre;
+    }
+    calcularImpuesto() {
+      return (this._impuesto._monto_bruto_anual - this._impuesto._deducciones) * 0.21;
+    }
 
-}
+  }
